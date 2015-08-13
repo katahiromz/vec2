0ada40b07091e13a837c1949e4923aa6c662bc65
Hirofumi Katayama <katayama.hirofumi.mz@gmail.com>
2015-08-13 12:47:51 +0900

    replace stdbool.h with pstdbool.h

--- vec2.h
+++ vec2.h
@@ -13,7 +13,7 @@
     #include <stdlib.h>
     #include <string.h>
     #include <assert.h>
-    #include <stdbool.h>    /* for bool, false, true */
+    #include "pstdbool.h"
 #endif
 
 /****************************************************************************/
